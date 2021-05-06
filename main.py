9e1b2294514da3ea7084ad32848ebd572af1140b
Naoto Ueno <naoto.ueno@ablecomputer.co.jp>
2021-05-06 21:42:48 +0900

    main file was modified to start after mysqld  started.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from datetime import datetime
 from apiClient import ApiClient
 import configparser
 import db
+import binascii
+
 lcd = lcd_class.LCD()
 OFF = 12
 ini = configparser.ConfigParser()
@@ -18,9 +20,18 @@ dbHost = ini['mysql']['host']
 dbName = ini['mysql']['db']
 user = ini['mysql']['user']
 passwd = ini['mysql']['passwd']
+print('test0')
 
-dbObj = db.DB(dbHost, dbName, user, passwd)
+dbObj = None
+while(dbObj is None):
+    try:
+        dbObj = db.DB(dbHost, dbName, user, passwd)
+        break
+    except:
+        dbObj = None
+        time.sleep(10)
 
+print('test1')
 def sendData(code, id, values):
     client = ApiClient(apiPath)
     params = client.createParams(code, id, values)
@@ -61,17 +72,18 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(OFF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(OFF, GPIO.FALLING)
 GPIO.add_event_callback(OFF, switchDown)
-
-port = serial.Serial("/dev/ttyUSB0", 115200, timeout=10)
-
+print('test')
+#port = serial.Serial("/dev/ttyUSB0", 115200, timeout=10)
+port = serial.Serial("/dev/serial0", 115200, timeout=0.5)
 lcd.string("System started!!", lcd.LINE_1)
 while True:
     try:
         line = port.readline()
+        #line = binascii.b2a_hex(line)
         print(line)
         line = line.decode('utf8')
         d = line.split(";")
-        
+        print(len(d))
         if(len(d) == 13):
             print(len(d))
             print('receive')
